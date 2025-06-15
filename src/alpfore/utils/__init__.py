7b233ea20735e2b7e5a86e29104a2ead88905449
Nicholas Stephen Herringer <nherringer@midway3-login4.rcc.local>
2025-06-15 15:12:05 -0500

    Add utils module with dataset_utils

---
+++ src/alpfore/utils/__init__.py
@@ -0,0 +1,14 @@
+from .dataset_utils import (
+    make_labeled_dataset,
+    save_labeled_dataset,
+    load_labeled_dataset,
+    append_new_data,
+)
+
+__all__ = [
+    "make_labeled_dataset",
+    "save_labeled_dataset",
+    "load_labeled_dataset",
+    "append_new_data",
+]
+
